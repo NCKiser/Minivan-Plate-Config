be1ba1b512e3e3a244372694f5a9d6e973e4ff83
Avi Avianto <avi.avianto@hexagon.com>
2023-04-25 17:25:05 -0400

    python again

--- dxf2png.py
+++ dxf2png.py
@@ -1,47 +1,37 @@
+import ezdxf
+import argparse
 import os
-import dxfgrabber
-from PIL import Image
 
-# Path to the folder containing the input DXF files
-input_folder = "all_dxf/"
+def convert_dxf_to_png(input_file, output_file):
+    dxf = ezdxf.readfile(input_file)
+    modelspace = dxf.modelspace()
 
-# Path to the folder where the output PNG files will be saved
-output_folder = "all_preview/"
+    if '$EXTMIN' in dxf.header:
+        x_min, y_min, _ = dxf.header['$EXTMIN']
+    else:
+        x_min, y_min = 0, 0
 
-# Create the output folder if it does not already exist
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
+    if '$EXTMAX' in dxf.header:
+        x_max, y_max, _ = dxf.header['$EXTMAX']
+    else:
+        x_max, y_max = 100, 100
 
-# Loop through all the DXF files in the input folder
-for filename in os.listdir(input_folder):
-    if filename.endswith(".dxf"):
-        # Load the DXF file
-        dxf = dxfgrabber.readfile(input_folder + filename)
+    # scale factor to fit drawing within a 1024x1024 image
+    scale_factor = 1024 / max(x_max - x_min, y_max - y_min)
 
-        # Get the bounding box of the drawing
-        x_min, y_min, x_max, y_max = dxf.header['$EXTMIN'], dxf.header['$EXTMAX']
+    # create a new DXF drawing with scaled entities
+    scaled_dxf = ezdxf.new('R2010')
+    msp = scaled_dxf.modelspace()
+    for entity in modelspace:
+        msp.add_entity(entity.copy().scale(scale_factor))
 
-        # Calculate the width and height of the drawing in pixels
-        width = int((x_max - x_min) * 100)
-        height = int((y_max - y_min) * 100)
+    # save the drawing to a PNG file
+    scaled_dxf.saveas(output_file)
 
-        # Create a new image with the calculated dimensions
-        img = Image.new("RGB", (width, height), color=(255, 255, 255))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_file', help='path to input DXF file')
+    parser.add_argument('output_file', help='path to output PNG file')
+    args = parser.parse_args()
 
-        # Draw the entities in the drawing onto the image
-        for entity in dxf.entities:
-            if entity.dxftype == 'LINE':
-                x1 = int((entity.start[0] - x_min) * 100)
-                y1 = int((entity.start[1] - y_min) * 100)
-                x2 = int((entity.end[0] - x_min) * 100)
-                y2 = int((entity.end[1] - y_min) * 100)
-                img_draw = ImageDraw.Draw(img)
-                img_draw.line([(x1, height - y1), (x2, height - y2)], fill=(0, 0, 0), width=1)
-
-        # Save the image as a PNG file
-        output_filename = os.path.splitext(filename)[0] + ".png"
-        img.save(output_folder + output_filename)
-
-        print(f"{filename} converted to {output_filename}")
-
-print("Conversion complete.")
+    convert_dxf_to_png(args.input_file, args.output_file)
