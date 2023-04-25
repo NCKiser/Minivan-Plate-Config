ac7f196b0a738973df6a81d1e1c261ac2fa9a311
Avi Avianto <avi.avianto@hexagon.com>
2023-04-25 17:16:46 -0400

    python again

--- dxf2png.py
+++ dxf2png.py
@@ -1,42 +1,23 @@
-import os
-import argparse
 import ezdxf
-from PIL import Image
+import os
 
 def convert_dxf_to_png(input_file, output_file):
-    # Load the DXF file
     doc = ezdxf.readfile(input_file)
-
-    # Get the Modelspace
     msp = doc.modelspace()
+    ext = msp.extmax
+    doc.set_modelspace_vport(0, ext)
+    doc.saveas(output_file)
 
-    # Get the bounding box of the Modelspace
-    bbox = msp.bbox()
+if not os.path.exists('all_preview'):
+    os.makedirs('all_preview')
 
-    # Calculate the size of the PNG image
-    width = int(bbox.width)
-    height = int(bbox.height)
-
-    # Create a new PIL image
-    image = Image.new('RGBA', (width, height), (255, 255, 255, 0))
-
-    # Create a PIL drawing context
-    drawing = ImageDraw.Draw(image)
-
-    # Iterate over all entities in the Modelspace
-    for entity in msp:
-        if entity.dxftype() == 'LINE':
-            start = entity.dxf.start
-            end = entity.dxf.end
-            drawing.line((start[0], height - start[1], end[0], height - end[1]), fill=(0, 0, 0, 255))
-
-    # Save the image to disk
-    image.save(output_file)
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Convert a DXF file to PNG.')
-    parser.add_argument('input_file', metavar='INPUT_FILE', help='the DXF file to convert')
-    parser.add_argument('output_file', metavar='OUTPUT_FILE', help='the PNG file to create')
-    args = parser.parse_args()
-
-    convert_dxf_to_png(args.input_file, args.output_file)
+for file in os.listdir('all_dxf'):
+    if file.endswith('.dxf'):
+        filename = os.path.splitext(file)[0]
+        dxf_file = os.path.join('all_dxf', file)
+        png_file = os.path.join('all_preview', f'{filename}.png')
+        if os.path.exists(png_file):
+            print(f'File {png_file} already exists, skipping.')
+        else:
+            print(f'Converting {dxf_file} to {png_file}')
+            convert_dxf_to_png(dxf_file, png_file)
