f5d4fefde7c9d966a46a13f22060c14704c6bdc0
Avi Avianto <avi.avianto@hexagon.com>
2023-04-25 17:33:07 -0400

    python again

--- dxf2png.py
+++ dxf2png.py
@@ -4,24 +4,54 @@ import os
 
 def convert_dxf_to_png(input_file, output_file):
     dxf = ezdxf.readfile(input_file)
-    msp = dxf.modelspace()
-    extmin, extmax = msp.get_extents()
-    width = abs(extmax[0] - extmin[0])
-    height = abs(extmax[1] - extmin[1])
-    png_width = 2048
-    png_height = int(height / width * png_width)
-    doc = freetype.Face(DEFAULT_FONT_PATH)
-    font_size = int(png_width / 30)
-    doc.set_char_size(size=font_size)
-    with open(output_file, 'wb') as png_file:
-        png_writer = png.Writer(png_width, png_height, greyscale=True)
-        pixels = []
-        for y in range(png_height):
-            for x in range(png_width):
-                p = get_point_from_pixel(x, y, png_width, png_height, extmin, extmax)
-                if p is not None:
-                    pixels.append(int(get_distance_to_nearest_entity(p, msp, doc) * 255))
-        png_writer.write_array(png_file, pixels)
+    modelspace = dxf.modelspace()
+
+    # Get bounding box
+    x_min, y_min, _, _ = modelspace.get_bbox()
+    _, _, x_max, y_max = modelspace.get_bbox()
+
+    # Set up image size
+    width = int((x_max - x_min) * SCALE_FACTOR)
+    height = int((y_max - y_min) * SCALE_FACTOR)
+
+    # Create the image
+    image = Image.new("RGBA", (width, height), (255, 255, 255, 0))
+    draw = ImageDraw.Draw(image)
+
+    # Draw the entities in the drawing
+    for entity in modelspace:
+        if entity.dxftype() == "LINE":
+            start = entity.dxf.start
+            end = entity.dxf.end
+            draw.line(
+                (
+                    (start[0] - x_min) * SCALE_FACTOR,
+                    height - (start[1] - y_min) * SCALE_FACTOR,
+                    (end[0] - x_min) * SCALE_FACTOR,
+                    height - (end[1] - y_min) * SCALE_FACTOR,
+                ),
+                fill=COLOR,
+                width=LINE_WIDTH,
+            )
+        elif entity.dxftype() == "LWPOLYLINE":
+            polyline = entity.get_points("xy")
+            for i in range(len(polyline) - 1):
+                start = polyline[i]
+                end = polyline[i + 1]
+                draw.line(
+                    (
+                        (start[0] - x_min) * SCALE_FACTOR,
+                        height - (start[1] - y_min) * SCALE_FACTOR,
+                        (end[0] - x_min) * SCALE_FACTOR,
+                        height - (end[1] - y_min) * SCALE_FACTOR,
+                    ),
+                    fill=COLOR,
+                    width=LINE_WIDTH,
+                )
+
+    # Save the image
+    image.save(output_file)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
