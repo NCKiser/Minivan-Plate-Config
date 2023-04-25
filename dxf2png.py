ba9c8cadcfbccb4fca2cf83235fcf83eaacb4a20
Avi Avianto <avi.avianto@hexagon.com>
2023-04-25 17:15:04 -0400

    python again

--- dxf2png.py
+++ dxf2png.py
@@ -1,29 +1,42 @@
 import os
-import sys
+import argparse
 import ezdxf
 from PIL import Image
 
 def convert_dxf_to_png(input_file, output_file):
+    # Load the DXF file
     doc = ezdxf.readfile(input_file)
+
+    # Get the Modelspace
     msp = doc.modelspace()
-    bbox = msp.get_bbox()
+
+    # Get the bounding box of the Modelspace
+    bbox = msp.bbox()
+
+    # Calculate the size of the PNG image
     width = int(bbox.width)
     height = int(bbox.height)
-    dwg_image = Image.new("RGB", (width, height), "white")
-    modelspace = doc.modelspace()
-    for entity in modelspace:
-        if entity.dxftype() == 'LINE':
-            start_point = entity.dxf.start
-            end_point = entity.dxf.end
-            x1 = int(start_point[0])
-            y1 = int(start_point[1])
-            x2 = int(end_point[0])
-            y2 = int(end_point[1])
-            dwg_image.putpixel((x1, y1), (0, 0, 0))
-            dwg_image.putpixel((x2, y2), (0, 0, 0))
-    dwg_image.save(output_file)
 
-if __name__ == "__main__":
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
-    convert_dxf_to_png(input_file, output_file)
+    # Create a new PIL image
+    image = Image.new('RGBA', (width, height), (255, 255, 255, 0))
+
+    # Create a PIL drawing context
+    drawing = ImageDraw.Draw(image)
+
+    # Iterate over all entities in the Modelspace
+    for entity in msp:
+        if entity.dxftype() == 'LINE':
+            start = entity.dxf.start
+            end = entity.dxf.end
+            drawing.line((start[0], height - start[1], end[0], height - end[1]), fill=(0, 0, 0, 255))
+
+    # Save the image to disk
+    image.save(output_file)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Convert a DXF file to PNG.')
+    parser.add_argument('input_file', metavar='INPUT_FILE', help='the DXF file to convert')
+    parser.add_argument('output_file', metavar='OUTPUT_FILE', help='the PNG file to create')
+    args = parser.parse_args()
+
+    convert_dxf_to_png(args.input_file, args.output_file)
