2552e50fbeb9ea428633fe8eed6e7d8e8cb2b33a
Avi Avianto <avi.avianto@hexagon.com>
2023-04-25 19:00:43 -0400

    rename

--- dxf2img.py
+++ dxf2img.py
@@ -1,43 +1,57 @@
-import matplotlib.pyplot as plt
+import os
+import re
+import glob
+import shutil
 import ezdxf
+import matplotlib.pyplot as plt
 from ezdxf.addons.drawing import RenderContext, Frontend
 from ezdxf.addons.drawing.matplotlib import MatplotlibBackend
-# import wx
-import glob
-import re
-import os
 
 
 class DXF2IMG(object):
 
-    default_img_format = '.png'
-    default_img_res = 300
-    def convert_dxf2img(self, names, img_format=default_img_format, img_res=default_img_res):
-        for name in names:
-            file_path = os.path.join('all_dxf', name)
-            doc = ezdxf.readfile(file_path)
-            msp = doc.modelspace()
-            # Recommended: audit & repair DXF document before rendering
-            auditor = doc.audit()
-            # The auditor.errors attribute stores severe errors,
-            # which *may* raise exceptions when rendering.
-            if len(auditor.errors) != 0:
-                raise exception("The DXF document is damaged and can't be converted!")
-            else :
-                fig = plt.figure()
-                ax = fig.add_axes([0, 0, 1, 1])
-                ctx = RenderContext(doc)
-                ctx.set_current_layout(msp)
-                ctx.current_layout.set_colors(bg='#FFFFFF')
-                out = MatplotlibBackend(ax)
-                Frontend(ctx, out).draw_layout(msp, finalize=True)
+    def __init__(self, img_format='.png', img_res=300):
+        self.img_format = img_format
+        self.img_res = img_res
 
-                img_name = re.findall("(\S+)\.",name)  # select the image name that is the same as the dxf file name
-                first_param = ''.join(img_name) + img_format  #concatenate list and string
-                fig.savefig(first_param, dpi=img_res)
+    def convert_dxf2img(self, dxf_file_path):
+        doc = ezdxf.readfile(dxf_file_path)
+        msp = doc.modelspace()
+        # Recommended: audit & repair DXF document before rendering
+        auditor = doc.audit()
+        # The auditor.errors attribute stores severe errors,
+        # which *may* raise exceptions when rendering.
+        if len(auditor.errors) != 0:
+            raise Exception("The DXF document is damaged and can't be converted!")
+        else:
+            fig = plt.figure()
+            ax = fig.add_axes([0, 0, 1, 1])
+            ctx = RenderContext(doc)
+            ctx.set_current_layout(msp)
+            ctx.current_layout.set_colors(bg='#FFFFFF')
+            out = MatplotlibBackend(ax)
+            Frontend(ctx, out).draw_layout(msp, finalize=True)
+
+            img_name = re.findall("(\S+)\.", os.path.basename(dxf_file_path))[0]  # select the image name that is the same as the dxf file name
+            img_path = os.path.join('all_preview', img_name + self.img_format)
+            fig.savefig(img_path, dpi=self.img_res)
+            plt.close(fig)
 
 
 if __name__ == '__main__':
-    first =  DXF2IMG()
-    first.convert_dxf2img(['GT-010.DXF'],img_format='.png')
+    dxf_folder = 'all_dxf'
+    img_folder = 'all_preview'
 
+    if not os.path.exists(img_folder):
+        os.makedirs(img_folder)
+
+    converter = DXF2IMG()
+    for dxf_file in glob.glob(os.path.join(dxf_folder, '*.dxf')):
+        converter.convert_dxf2img(dxf_file)
+        print(f'{dxf_file} converted to an image')
+
+        # move the converted image to the preview folder
+        img_name = re.findall("(\S+)\.", os.path.basename(dxf_file))[0] + converter.img_format
+        img_path = os.path.join(img_folder, img_name)
+        shutil.move(img_name, img_path)
+        print(f'{img_name} moved to {img_path}')
