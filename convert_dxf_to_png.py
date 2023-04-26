c1bd7dfdb63b1bd7bae313383c7b610f30fdcda6
avianto <dev@avianto.com>
2023-04-25 21:30:47 -0400

    check file name

--- convert_dxf_to_png.py
+++ convert_dxf_to_png.py
@@ -1,38 +1,47 @@
+import os
+import re
+import argparse
+import logging
+import matplotlib.pyplot as plt
+import ezdxf
+from ezdxf.addons.drawing import RenderContext, Frontend
+from ezdxf.addons.drawing.matplotlib import MatplotlibBackend
+
+logging.basicConfig(level=logging.INFO)
+
+
 class DXF2IMG(object):
-    
+
     def __init__(self, img_format, img_res):
         self.img_format = img_format
         self.img_res = img_res
-    
+
     def convert_dxf2img(self, dxf_path, preview_path):
         logging.info(f"Converting DXF files in {dxf_path} to {self.img_format.upper()} at {self.img_res} DPI")
         if not os.path.exists(dxf_path):
             logging.error(f"{dxf_path} does not exist!")
             return
-        
+
         if not os.path.isdir(dxf_path):
             logging.error(f"{dxf_path} is not a directory!")
             return
-        
+
         os.makedirs(preview_path, exist_ok=True)
-        
-        dxf_pattern = re.compile(r".+\.dxf$", re.IGNORECASE)
+
         for filename in os.listdir(dxf_path):
-            if not dxf_pattern.match(filename):
-                logging.warning(f"{filename} is not a DXF file, skipping")
-                continue
-            
             dxf_file = os.path.join(dxf_path, filename)
-            img_file = os.path.join(preview_path, os.path.splitext(filename)[0] + self.img_format)
-            
-            if os.path.isfile(img_file):
+
+            if not dxf_file.lower().endswith(".dxf"):
+                logging.warning(f"{dxf_file} is not a DXF file, skipping")
+                continue
+
+            img_file = os.path.join(preview_path, os.path.splitext(filename)[0])
+            # img_file = os.path.join(preview_path, os.path.splitext(filename)[0] + self.img_format)
+
+            if os.path.exists(img_file):
                 logging.warning(f"{img_file} already exists, skipping")
                 continue
-            
-            if not os.path.isfile(dxf_file):
-                logging.warning(f"{dxf_file} does not exist, skipping")
-                continue
-            
+
             try:
                 doc = ezdxf.readfile(dxf_file)
                 msp = doc.modelspace()
@@ -50,10 +59,21 @@ class DXF2IMG(object):
                     # ctx.current_layout.set_colors(bg='#FFFFFF')
                     out = MatplotlibBackend(ax)
                     Frontend(ctx, out).draw_layout(msp, finalize=True)
-                    
+
                     fig.savefig(img_file, dpi=self.img_res)
                     logging.info(f"Converted {dxf_file} to {img_file}")
             except Exception as e:
                 logging.error(f"Error converting {dxf_file}: {e}")
-        
+
         logging.info("Conversion complete")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Convert DXF files to PNG')
+    parser.add_argument('dxf_dir', help='Directory containing DXF files')
+    parser.add_argument('--img-format', default='.png', help='Output image format (default: .png)')
+    parser.add_argument('--img-res', type=int, default=300, help='Output image resolution (default: 300)')
+    args = parser.parse_args()
+
+    converter = DXF2IMG(args.img_format, args.img_res)
+    converter.convert_dxf2img(args.dxf_dir, os.path.join(os.getcwd(), 'all_preview'))
